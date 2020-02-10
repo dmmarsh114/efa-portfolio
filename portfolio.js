d68b6e3c2daf7385d8346d5d6b1c3af10c96a08c
dmmarsh114 <49419067+dmmarsh114@users.noreply.github.com>
2020-02-10 15:44:21 -0500

    what am I doing

---
+++ portfolio.js
@@ -0,0 +1,22 @@
+// when the user scrolls the page, execute headerScroll
+window.onscroll = function() { navbarScroll() };
+
+// the header element
+var header = document.getElementById("myNavbar");
+
+// offsetTop is the position of the header
+var sticky = header.offsetTop;
+
+// add the sticky class to the header when the user scrolls 
+function navbarScroll() {
+  // if the user scrolls (pageYOffset) past the header's position, add the sticky class
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    header.classList.add("navbar-dark");
+    header.classList.add("bg-dark");
+  } else {
+    header.classList.remove("sticky");
+    header.classList.remove("navbar-dark");
+    header.classList.remove("bg-dark");
+  }
+}
